32cc4523718e9576e2f7631205af663f0e44b275
wangweihua <aztack@163.com>
2018-04-26 15:08:48 +0800

    bigbang

---
+++ comp-draggable.js
@@ -0,0 +1,13 @@
+cc.Class({
+  name: 'Draggable[拖拽]',
+  extends: cc.Component,
+  editor: CC_EDITOR && {
+    help: 'https://code.vipkid.com.cn/lingobus-fe/games/library/comp-draggable/wikis/home'
+  },
+  properties: {
+    enabled: true
+  },
+  onLoad: function() {
+
+  }
+});
